77cadb34a3002ed639b33690e68c21f1c0756812
SlotH <implimail@gmail.com>
2021-11-08 23:51:45 +0100

    Finish second section of sixth chapter from RustBook

--- enums/src/main.rs
+++ enums/src/main.rs
@@ -49,6 +49,49 @@ struct _MoveMessage {
 struct _WriteMessage(String); // tuple struct
 struct _ChangeColorMessage(i32, i32, i32); // tuple struct
 
+enum Coin {
+    Penny, 
+    Nickel,
+    _Dime,
+    Quarter(UsState),
+}
+
+impl Coin {
+    fn value_in_cents(&self) -> u8{
+        // difference between if and match: if always needs a boolean, 
+        // match can handle all data types
+        match self {
+            // these are all match arms
+            // each arm has a pattern and a code
+            // => seperates the pattern from the code to be executed
+            Coin::Penny => 1, // in this case the code only returns 1
+            // additional code can be executed in an arm's code, but the
+            // defined data type has to be returned:
+            Coin::Nickel => {
+                println!("\nWe got a winner!");
+                5
+            },
+            Coin::_Dime => 10,
+            // the quarter also has the UsState value, which can be used in the match arm
+            Coin::Quarter(state) => {
+                println!("You just entered a Quarter from the state of {:?}", state);
+                25
+            } 
+        }
+    }
+    
+}
+
+// binding patterns to values is useful to extract values out of enum variants
+// only quarters do have the UsState enum value 
+#[derive(Debug)]
+#[allow(dead_code)]
+#[allow(non_camel_case_types)]
+enum UsState {
+    // normally this shouldn't be done like this..., but for practice purposes fine
+    Alabama, Alaska, American_Samoa, Arizona, Arkansas, California, Colorado, Connecticut, Delaware, District_of_Columbia, Florida, Georgia, Guam, Hawaii, Idaho, Illinois, Indiana, Iowa, Kansas, Kentucky, Louisiana, Maine, Maryland, Massachusetts, Michigan, Minnesota, Minor_Outlying_Islands, Mississippi, Missouri, Montana, Nebraska, Nevada, New_Hampshire, New_Jersey, New_Mexico, New_York, North_Carolina, North_Dakota, Northern_Mariana_Islands, Ohio, Oklahoma, Oregon, Pennsylvania, Puerto_Rico, Rhode_Island, South_Carolina, South_Dakota, Tennessee, Texas, US_Virgin_Islands, Utah, Vermont, Virginia, Washington, West_Virginia, Wisconsin, Wyoming
+}
+
 fn main() {
     println!("6.1.0 - Defining an enum");
     // Instances of the IpAddrType:
@@ -99,9 +142,118 @@ fn main() {
     // When working with Option<T> → we need to check (and compiler makes us check)
     // for an invalid value
 
+    println!("\n6.2.0 - match");
+    // match allows to execute code, dependent on pattern matching
+    // the compiler checks if all possible values are handled
+    // Check for this example the enum and method out of scope 
+    let one_cent = Coin::Penny;
+    let five_cents = Coin::Nickel;
+
+    println!("The first coin was: {} cent(s)", one_cent.value_in_cents());
+    println!("The second coin was: {} cent(s)", five_cents.value_in_cents());
+
+    println!("\n6.2.1 - Binding patterns to values");
+    // match arms can bind to the parts of values that match the pattern 
+    let twentyfive_cents = Coin::Quarter(UsState::New_York);
+    println!("The third coin was: {} cent(s)", twentyfive_cents.value_in_cents());
+
+    println!("\n6.2.2 - match Option");
+    // the principle remains the same of match and option (just used for exercise)
+    let some_number = Some(5);
+    let absent_number: Option<i32> = None;
+
+    println!("Returned plus one call from some_number: {:?}", plus_one_if_some(some_number));
+    println!("Returned plus one call form absent_number: {:?}", plus_one_if_some(absent_number));
+
+    println!("6.2.3 - Matches are Exhaustive!!");
+    // when not handling all values of an enum, the code will not compile. For example:
+    // In this case None was not handled
+    // match x {
+    //    Some(i) => i,
+    // }
+    // i.e.: each value shall be handled (this prevents the Null problem!)
+    
+    println!("\n6.2.4 - Catch-all");
+    // one usecase: only one option is important, all other values shall return a default:
+    #[allow(dead_code)]
+    #[derive(Debug)]
+    enum DiceRoll {
+        Two, Three, Four, Five, Six, Seven, Eight, Nine, Ten, Eleven, Twelve,
+    }
+
+    // The last arm (if it is not a value of the enum / options) is the default
+    // Other could have any other variable name
+    // no additional arm is allowed after the other (default) arm 
+    impl DiceRoll {
+        fn evaluate_dice_roll(&self) {
+            match self {
+                DiceRoll::Three => println!("You just got a hat!!"),
+                DiceRoll::Seven => println!("Sorry, you just lot your hat"),
+                other => println!("Move {:?} fields", other),
+            }
+        }
+    }
+
+    let roll_1 = DiceRoll::Three;
+    let roll_2 = DiceRoll::Seven;
+    let roll_3 = DiceRoll::Eight;
+    let roll_4 = DiceRoll::Ten;
+
+    roll_1.evaluate_dice_roll();
+    roll_2.evaluate_dice_roll();
+    roll_3.evaluate_dice_roll();
+    roll_4.evaluate_dice_roll();
+
+    println!("\n6.2.5 - Ignoring rest operator _");
+    // Similiar can be achieved, if we do not need to use the actual default value
+    // i.e.: we do not care about the actual value but need to define an action 
+    // In this case underscore _ is required
+    impl DiceRoll {
+        fn evaluate_dice_roll_with_reroll(&self) {
+            match self {
+                DiceRoll::Three => println!("You just got a hat!!"),
+                DiceRoll::Seven => println!("Sorry, you just lot your hat"),
+                _ => println!("You did not roll 3 or 7, you have to reroll."),
+            }
+        }
+
+        fn tell_rust_that_definitely_one_other_arm_is_used(&self) {
+            println!("\nIgnoring, if not Seven (7): ");
+            match self {
+                DiceRoll::Seven => println!("Really lucky. All other numbers are not used!!!"),
+                // Following line tells rust that one of the arms before has to be used
+                // Otherwise the code shall not run
+                _ => (),
+            }
+        }
+    }
+
+    let roll_1 = DiceRoll::Three;
+    let roll_2 = DiceRoll::Seven;
+    let roll_3 = DiceRoll::Eight;
+    let roll_4 = DiceRoll::Ten;
+
+    roll_1.evaluate_dice_roll_with_reroll();
+    roll_2.evaluate_dice_roll_with_reroll();
+    roll_3.evaluate_dice_roll_with_reroll();
+    roll_4.evaluate_dice_roll_with_reroll();
+
+    roll_2.tell_rust_that_definitely_one_other_arm_is_used();
+    roll_4.tell_rust_that_definitely_one_other_arm_is_used();
 }
+
+// Function for 6.1.0
 // This function can now be called with all IpAddrType options:
 fn route(ip_type: IpAddrType) {
     println!("We chose IP address type: {:?}.", ip_type);
 
+}
+
+// Function for 6.2.2.
+// This function is used as exercise for Option matching
+fn plus_one_if_some(maybe_number: Option<i32>) -> Option<i32> {
+    match maybe_number {
+        Some(number) => Some(number + 1),
+        None => None,
+    }
 }
