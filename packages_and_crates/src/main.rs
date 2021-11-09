e76c6cfdb20e996a72df06c738434c1e332b274f
SlotH <implimail@gmail.com>
2021-11-09 21:07:26 +0100

    Finish introduction of chapter 7 from RustBook

---
+++ packages_and_crates/src/main.rs
@@ -0,0 +1,12 @@
+fn main() {
+    // Projects can be split into: multiple modules
+    // Modules can be split into: multiple files
+    // A package can contain multiple binary crates and one optional library crate
+    /*
+        Module system:
+        Packages: Cargo feature that lets you build, test, and share crates
+        Crates: A tree of modules that produces a library or executable
+        Modules & use: Let you control the organization, scope, and privacy of paths
+        Paths: A way of naming an item, such as a struct, function, or module
+    */
+}
