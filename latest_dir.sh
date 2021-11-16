ee5fdc5f5a595a086229094600fec854f7deaea8
SlothSky <implimail@gmail.com>
2021-11-16 11:03:37 +0100

    Add logging to latest_dir.sh [collections]

--- latest_dir.sh
+++ latest_dir.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 # get the commit message's last element
 GITLOG=$(git log -1 --pretty=format:"%s")
-echo $GITLOG
+echo "LAST COMMIT MESSAGE: ${GITLOG}"
 LOG_ELEMENTS=' ' read -r -a array <<< "$GITLOG"
 directory="${array[-1]}"
 
 # remove the brackets from the last element
 directory_name=${directory#"["}
 directory_name=${directory_name::-1}
+echo "USED DIRECTORY FOR BUILD: ${directory_name}"
 cd $directory_name
+
+# after changing to latest directory build software
+echo "BUILDING SW"
+cargo build --verbose
+echo "RUNNING SW"
+cargo run
