411dd5c5b71749f6f4ff7f5f4a702eec6bd3c8cf
SlotH <jan.weiss5@gmail.com>
2022-06-21 23:51:20 +0200

    Finish second second of chapter 10 from RustBook [generics]

---
+++ generics/src/lib.rs
@@ -0,0 +1,104 @@
+use std::fmt::Display;
+
+// struct definitions required for 10.2.2
+pub struct GasStation {
+    pub pump_amount: u32,
+    pub gas_price: f32,
+    pub diesel_price: f32,
+    pub name: String,
+}
+
+pub struct ChargingStation {
+    pub pump_amount: u32,
+    pub slow_charging_price: f32,
+    pub fast_charging_price: f32,
+    pub name: String,
+}
+
+pub struct UnitFillUpStation;
+    
+// println!("\n\n10.2.0 Traits: Defining Shared Behaviour");
+// traits are defines functionalities of a specific type, which it 
+// can share with other types
+// trait bounds specify that a generic type can be any type w/ a certain behaviour
+
+// println!("\n10.2.1 Defining traits");
+// Behaviour of a type == the methods of this type
+// Shared behaviour == Same method can be called on multiple types
+// Trait definition == Set of method signatures for accomplishing some specific purpose
+
+// defining the Summary trait
+pub trait Summary {
+    // each type that is implementing this trait must define its own behaviour for the body
+    fn average_price(&self) -> f32;
+    // multiple method signatures would be allowed here
+    // following here is a default implementation of a trait behaviour
+    fn announcement_generator(&self) -> String {
+        format!(
+            "This fill up station which is called is selling on an average of {}",
+            self.average_price()
+        )
+    }
+    // default implementations can call other methods off the same trait
+}
+
+// println!("\n10.2.2 Implementing a trait on a type");
+// this way, the default implematation is implemented for GasStation as well
+impl Summary for GasStation {
+   fn average_price(&self) -> f32 {
+       (&self.diesel_price + &self.gas_price) / 2.0
+   } 
+}
+
+impl Summary for ChargingStation {
+    fn average_price(&self) -> f32 {
+        (&self.fast_charging_price + &self.slow_charging_price) / 2.0
+    }
+}
+
+impl Summary for UnitFillUpStation {
+    fn average_price(&self) -> f32 {
+        99.99
+    }
+}
+
+// in order to use the default implementation 'name' of the Summary trait, use empty impl block
+// impl Summary for GasStation {}
+// in this case the Summary implementation for CharingStation would still be valid
+
+// conditionally implementing methods on types depending on trait bounds of the generic types:
+struct Pair<T> {
+    x: T,
+    y: T,
+}
+
+// the new method is implemented for all specific types
+impl<T> Pair<T> {
+    fn new(x: T, y: T) -> Self {
+        Self { x, y }
+    }
+}
+
+// this method is only implemented for the specific types that implement the traits Display
+// and PartialOrd
+impl<T: Display + PartialOrd> Pair<T> {
+    fn cmp_display(&self) {
+        if self.x >= self.y {
+            println!("X is bigger than Y");
+        } else {
+            println!("Y is bigger than X");
+        }
+    }
+}
+
+// blanked implementations are trait definitions conditioned on other trait definitions:
+// one example from the standard library
+/*
+    impl<T: Display> ToString for T { ... }
+*/
+
+
+
+
+
+
--- generics/src/main.rs
+++ generics/src/main.rs
@@ -1,39 +1,42 @@
+use std::{fmt::{Display, Debug}};
+
+use generics::{GasStation, ChargingStation, Summary};
 // both properties of the struct must in this case be of the same type, if both
 // are of type T
 struct Point<T> {
-    x: T,
-    y: T,
+    _x: T,
+    _y: T,
 }
 
 // the <T> after the impl, tells Rust that the <T> after Point is a generic type and not a concrete type
 impl<T> Point<T> {
-    fn x(&self) -> &T {
-        &self.x
+    fn _x(&self) -> &T {
+        &self._x
     }
 }
 
 // constraints can be used for generic structs by implementing a non-generic method:
 // e.g. this method is only valid for the f32 version of Point<T>
 impl Point<f32> {
-    fn distance_from_origin(&self) -> f32 {
-        (self.x.powi(2) + self.y.powi(2)).sqrt()
-    }
-}
-
-// method definitions themselves can also contain generic types
-impl<T, U> SecondPoint<T, U> {
-    fn mixup_points<V, W>(self, other_point: SecondPoint<V, W>) -> SecondPoint<T, W> {
-        SecondPoint { 
-            x: self.x, 
-            y: other_point.y,
-        }
+    fn _distance_from_origin(&self) -> f32 {
+        (self._x.powi(2) + self._y.powi(2)).sqrt()
     }
 }
 
 // if both types shall be generic, but from different types, we can define two generic data types:
-struct SecondPoint<T, U> {
+struct _SecondPoint<T, U> {
     x: T,
     y: U,
+}
+
+// method definitions themselves can also contain generic types
+impl<T, U> _SecondPoint<T, U> {
+    fn _mixup_points<V, W>(self, other_point: _SecondPoint<V, W>) -> _SecondPoint<T, W> {
+        _SecondPoint { 
+            x: self.x, 
+            y: other_point.y,
+        }
+    }
 }
 
 fn main() {
@@ -52,7 +55,7 @@ fn main() {
     let char_vec = vec!['a', 'A', 'g', 'Y'];
     println!("The largest char value in the given list is: {:?}", largest_char(&char_vec));
 
-    // more efficient: generix function call:
+    // more efficient: generic function call:
     println!("The largest i32 value deceived by the generic function: {:?}", largest(&i32_vec));
     println!("The largest char value deceived by the same generic function {:?}", largest(&char_vec));
 
@@ -60,8 +63,8 @@ fn main() {
     // struct definitions can be based on a generic type via <>
    // see for example struct defined in the top rows
     // consequence is that following is allowed:
-    let int_point = Point { x: 3, y: 10 };
-    let float_point = Point { x: 2.4, y: 9.9 };
+    let _int_point = Point { _x: 3, _y: 10 };
+    let _float_point = Point { _x: 2.4, _y: 9.9 };
 
     println!("\n10.1.2 generic data types in enum definitions");
     // enums can be defined over a generic type as well
@@ -74,6 +77,60 @@ fn main() {
     println!("\n10.1.4 performance of code which is using generics");
     // RUNNING THE CODE W/ GENERICS DOES NOT MAKE IT SLOWER THAN USING CONCRETE DATA TYPES
     // BUZZ WORD: monomorphization
+
+    println!("\n\n10.2.0 Traits: Defining Shared Behaviour");
+    // traits are defines functionalities of a specific type, which it 
+    // can share with other types
+    // trait bounds specify that a generic type can be any type w/ a certain behaviour
+
+    println!("\n10.2.1 Defining traits");
+    // Behaviour of a type == the methods of this type
+    // Shared behaviour == Same method can be called on multiple types
+    // Trait definition == Set of method signatures for accomplishing some specific purpose
+
+    println!("\n10.2.2 Implementing a trait on a type");
+    // note the structs and the trait first need to be brought into scope (top lines) 
+    let aral = GasStation {
+        pump_amount: 8,
+        gas_price: 1.80,
+        diesel_price: 1.90,
+        name: String::from("ARAL"),
+    };
+
+    println!("The average price of the {} gas station is {}", aral.name, aral.average_price());
+    // traits can be only implemented when either the type, or the trait definition is local
+    // or if both are local (e.g. here Summary and GasStation are locally defined)
+    // Implementing w/ two external implementations is not possible (e.g. Vec<T> w/ Display)
+
+    println!("\n10.2.3 Default trait implementations");
+    // Trait behaviour can be defined as default on trait implementations
+    // When implementing this trait on a specific type, this behaviour can be overwritten
+    // Check lib.rs for example default implementation (capacity)
+    println!("Welcome, {}", aral.announcement_generator());
+
+    println!("\n10.2.4 Traits as parameters");
+    // Traits can be used for function definitions in order to request a generic type:
+    // This function shall get some type that implements the following trait
+    // see function definition below
+    let bianco_charging = ChargingStation {
+        pump_amount: 12,
+        fast_charging_price: 0.20,
+        slow_charging_price: 0.15,
+        name: String::from("biancoCharging"),
+    };
+
+    alert(&bianco_charging);
+
+    println!("\n10.2.5 Trait bound syntax");
+    // impl Trait is a short hand for the actual 'trait bound' syntax
+    // see in the function below
+    println!("Check section about trait bounds below in this file");
+
+    println!("\n10.2.6 Returning types that are implementing traits");
+    // check function below for function which are returning types that implement traits
+
+    println!("\n10.2.7 Conditionally implementing methods based on trait bounds");
+    // See lib.rs for examples
 }
 
 // function for 10.1.0 
@@ -106,7 +163,11 @@ fn largest_char(list: &[char]) -> char {
 // T is used as type parameter by convention (any other name would be allowed)
 // type name declarations are put into <> between name of the function and the parameter list
 // i.e. the function largest is generic over the type T
-fn largest<T>(list: &[T]) -> T {
+/*
+    EDIT: after 10.2.6 following solution can be implemented
+    - the generic type T requires a trait bound (for the > handling)
+*/
+fn largest<T: PartialOrd + Copy>(list: &[T]) -> T {
     let mut largest = list[0];
 
     for &item in list {
@@ -117,3 +178,78 @@ fn largest<T>(list: &[T]) -> T {
 
     largest
 }
+
+// functions for 10.2.4 → traits as parameters
+pub fn alert(price: &impl Summary) {
+    println!("Alert! The current average price for filling up is at: {}", price.average_price());
+}
+// the impl trait syntax is the short form of the trait bound: 
+// the trait bound in this example is the Summary after the colons
+pub fn long_alert_version<T: Summary>(price: &T) {
+    println!("Alert! The current average price for filling up is at: {}", price.average_price());
+}
+
+// if multiple parameters are defined it can be enforced that both are from the same type
+pub fn multi_alert<T: Summary>(price1: &T, _price2: &T) {
+    println!("Alert! The current average price for filling up is at: {}", price1.average_price());
+}
+// if multiple parameters are defined, but the type does not matter:
+pub fn multi_alert_short(price1: &impl Summary, _price2: &impl Summary) {
+    println!("Alert! The current average price for filling up is at: {}", price1.average_price());
+}
+
+// multiple trait bonds for one parameter:
+// the given parameter type must implement both traits:
+pub fn alert_and_display_short(_price: &(impl Summary + Display)) {
+    // do something here with the two traits
+}
+// same for trait bounds:
+pub fn alert_and_display_long<T: Summary + Display>(_price: &T) {
+    // do something here with the two traits
+}
+
+// function signatures can get confusing when each generic type parameter comes w/ its
+// own set of trait bounds
+// alternative 'where' syntax is described in the simple_version:
+fn _complicated_version<T: Display + Clone, U: Debug + Clone>(_t: &T, _u: &U) -> i32 {
+    // do something here
+    42
+}
+
+fn _simple_version<T, U>(_t: &T, _u: &U) -> i32 
+    where T: Display + Clone, 
+          U: Debug + Clone
+{
+    // do something here
+    42
+}
+
+// functions for 10.2.6
+// this following function returns some type that implements the Summary trait
+// therefore no long type definition has to be provided
+// BUT ONLY ONE TYPE THAT IMPLEMENTS SUMMARY MUST BE RETURNED
+/* 
+// FOLLOWING CODDE WOULD THEREFORE NOT COMPILE: GasStation ↔ ChargingStations
+fn returns_fillup_station(car_model: String) -> impl Summary {
+    if car_model.as_str() == "911" {
+        // FOLLOWING CODE WOULD NOT COMPILE: GasStation ↔ ChargingStation
+        GasStation { 
+            name: String::from("Aral"), 
+            gas_price: 1.87, 
+            diesel_price: 1.91, 
+            pump_amount: 4, 
+        }
+    } else {
+        ChargingStation {
+            name: String::from("biancoCharging"),
+            fast_charging_price: 0.18,
+            slow_charging_price: 0.10,
+            pump_amount: 14, 
+        }
+    }
+    }
+*/
+
+
+
+
